2417adb27921454aa9d358f4567497f28b1a9f61
박우준 <parkwoojun@bag-ujuns-MacBook-Air.local>
2026-08-08 02:48:00 +0900

    feat: add coming soon pages for new arrivals and popular products

--- src/builders/flex/newArrivalMenu.ts
+++ src/builders/flex/newArrivalMenu.ts
@@ -1,49 +1,106 @@
-import { FlexMessage } from "../../types/line";
+import type { FlexMessage } from "../../types/line";
 
 export function createNewArrivalMenuFlex(): FlexMessage {
   return {
     type: "flex",
-    altText: "新品",
+    altText: "新品上市",
     contents: {
       type: "bubble",
-      hero: {
-        type: "image",
-        url: "https://placehold.co/1200x630",
-        size: "full",
-        aspectMode: "cover",
-        aspectRatio: "20:13",
-      },
+
       body: {
         type: "box",
         layout: "vertical",
+        paddingAll: "24px",
+        spacing: "md",
         contents: [
           {
             type: "text",
-            text: "🆕 新品專區",
+            text: "NEW ARRIVALS",
+            size: "xs",
+            color: "#999999",
             weight: "bold",
-            size: "xl",
           },
           {
             type: "text",
-            text: "最新上架的韓國商品",
-            margin: "md",
+            text: "新品上市",
+            size: "xl",
+            weight: "bold",
+            color: "#111111",
+          },
+          {
+            type: "text",
+            text: "新品專區即將上線",
+            size: "md",
+            weight: "bold",
+            color: "#333333",
+            margin: "lg",
+          },
+          {
+            type: "text",
+            text: "我們正在準備更多來自韓國的時尚、美妝與生活風格商品。",
+            size: "sm",
             color: "#666666",
             wrap: true,
+            lineSpacing: "4px",
+          },
+
+          {
+            type: "separator",
+            margin: "xl",
+            color: "#EEEEEE",
+          },
+
+          {
+            type: "box",
+            layout: "vertical",
+            margin: "lg",
+            paddingAll: "16px",
+            backgroundColor: "#F7F7F7",
+            cornerRadius: "12px",
+            spacing: "sm",
+            contents: [
+              {
+                type: "text",
+                text: "COMING SOON",
+                size: "xs",
+                weight: "bold",
+                color: "#999999",
+              },
+              {
+                type: "text",
+                text: "正式上線前",
+                size: "sm",
+                weight: "bold",
+                color: "#111111",
+              },
+              {
+                type: "text",
+                text: "您可以先前往 Seoul Moment 官方網站，查看目前販售中的精選商品。",
+                size: "xs",
+                color: "#777777",
+                wrap: true,
+                lineSpacing: "3px",
+              },
+            ],
           },
         ],
       },
+
       footer: {
         type: "box",
         layout: "vertical",
-        spacing: "sm",
+        paddingAll: "16px",
+        paddingTop: "4px",
         contents: [
           {
             type: "button",
             style: "primary",
+            height: "sm",
+            color: "#111111",
             action: {
               type: "uri",
-              label: "查看新品",
-              uri: "https://seoulmoment.com.tw/new",
+              label: "前往 Seoul Moment 官網",
+              uri: "https://seoulmoment.com.tw/",
             },
           },
         ],
